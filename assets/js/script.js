204e11cfa1a72b04b6deb508306e5974f73c7e41
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-04-25 09:33:18 -0500

    JS file has comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,26 @@
-// PSEUDO CODE!!!!
+// Arrays for the five quiz questions and sets of answers for choices 1, 2, 3, and 4
 
-// THE FIRST PAGE SHOULD HAVE A TITLE AND A BRIEF INTRODUCTION ABOUT TAKING A CODING RELATED QUIZ. THERE SHOULD BE A START BUTTON TO INITIATE THE QUIZ. 
-
-// THERE SHOULD BE FIVE QUESTIONS, EACH WITH AN ORDERED LIST OF ANSWERS FROM WHICH TO CHOOSE. WHEN YOU ANSWER A QUESTION, YOU SHOULD BE NOTIFIED WHETHER YOUR ANSWER WAS CORRECT OR WRONG. YOU SHOULD BE ABLE TO SEE A TIMER COUNTING DOWN.
-
-// IF YOU GET A QUESTION WRONG, SOME TIME SHOULD BE SUBTRACTED FROM THE CLOCK.
-
-// AFTER FIVE QUESTIONS, OR IF THE TIMER HITS ZERO, YOU SHOULD SEE AN "ALL DONE!" SORT OF MESSAGE, AND BELOW THAT SHOULD BE A DISPLAY OF YOUR SCORE, WHICH IS A POINT FOR EVERY SECOND REMAINING ON THE CLOCK, AND A FIELD WHERE YOU CAN ENTER YOUR INITIALS FOR THE HIGH SCORE LIST, ALONG WITH A SUBMIT BUTTON.
-
-// AFTER YOU HIT SUBMIT, YOU SHOULD BE TAKEN TO A SCOREBOARD THAT DISPLAYS HIGH SCORES WITH INITIALS, AND A BUTTON TO START THE QUIZ AGAIN
-
-// I WILL NEED EVENT LISTENERS FOR CLICKS ON THE ANSWERS. 
-
-// DO I CREATE ARRAYS FOR THE QUESTIONS, AND ARRAYS FOR ANSWERS 1, 2, 3, AND 4? OR ARRAYS FOR THE OPTIONS FOR Q1, OPTIONS FOR Q2, AND SO ON?
 const theQuestions = ["In an HTML document, where should you include reference to the JavaScript?", "How do you create a function in JavaScript?", "Which operator is used to assign a value to a variable?", "How would you call a function named myFunction?", "How would one write an if statement to execute code if the variable i is not equal to 5?"];
-console.log(theQuestions);
 const answersOne = ["The body section", "function = myFunction()", "Equal sign (=)", "call myFunction()", "if (i <> 5)"];
 const answersTwo = ["The head section", "function:myFunction()", "Asterisk (*)", "call function myFunction()", "if i <> 5"];
 const answersThree = ["Both the head section and the body section", "function myFunction()", "Hyphen (-)", "myFunction()", "if i =! 5 then"];
 const answersFour = ["Neither the head nor the body section", "Any of these is fine", "The letter x", "Any of these will call that function", "if (i != 5)"];
 const correctAnswers = ["The body section", "function myFunction()", "Equal sign (=)", "myFunction()", "if (i != 5)"];
 
+// Variables for the start button, the timer area, and a starting point for the variable that keeps track of how many questions have been answered. The latter is used in a for loop to run through the questions.
+
 var startButton = document.querySelector("#startButton");
 var timerEl = document.querySelector("#countdown");
 var questionsAnswered = 0;
+
+// This displays the intro text and turns off the display for other divs.
 
 document.querySelector("#intro").innerHTML = "We're going to ask you five questions about coding basics. A timer will count down while you do this. If you answer a question incorrectly, 15 seconds will be subtracted from the remaining time. Your final score will equal the number of seconds remaining at the end of the quiz.";
 quizContainer.style.display = "none";
 enterInits.style.display = "none";
 savedScores.style.display = "none";
 
+// Listener event for the user clicking on the start button
 
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
@@ -44,15 +35,18 @@ startButton.addEventListener("click", function(event) {
     document.querySelector("#optionFour").innerHTML = answersFour[0];
 })
 
+// Function for the timer
+
 function countdown() {
     var timeLeft = 75;
     grandTotal = timeLeft;
     var intervalId = setInterval(function() {
         timerEl.textContent = "Seconds remaining: " + timeLeft;
-        console.log(timerEl.textContent);
+        // Changes the timer text color to red once the time nears the end 
         if (timeLeft <= 15) {
             timerEl.setAttribute("style", "color: red;")
         }
+        // End the quiz if timer hits zero 
         if (timeLeft <= 0) {
             grandTotal = 0;
             clearInterval(intervalId);
@@ -61,6 +55,7 @@ function countdown() {
         timeLeft--;
     }, 1000)
 
+    // Adds listener event to all list items in the quiz; provides correct/incorrect messages in different colors after the user makes a choice 
     const answerOptions = document.querySelectorAll("li");
         answerOptions.forEach(li => {
             li.addEventListener("click", function(event){
@@ -74,14 +69,16 @@ function countdown() {
                     document.querySelector("#praise").textContent = "Last answer: WRONG! 15 POINTS LOST.";
                     timeLeft -= 15;
                 }
-            questionsAnswered++;
-            if (questionsAnswered === 5) {  
+                // Adds one to the questions answered variable 
+                questionsAnswered++;
+                // Ends the quiz if the user has answered all five questions, otherwise proceeds to next question 
+                if (questionsAnswered === 5) {  
                 grandTotal = timeLeft;
                 finalScore();
                 clearInterval(intervalId);
                 console.log("The game should end now.");
                 console.log(grandTotal);
-            } else {
+                } else {
                 nextQuestion()
             }
         })
@@ -90,7 +87,7 @@ function countdown() {
 
 
 
-
+// Loads the question and answer options for the next question 
 function nextQuestion() {
         document.querySelector("#questionHeader").innerHTML = theQuestions[questionsAnswered];
         document.querySelector("#optionOne").innerHTML = answersOne[questionsAnswered];
@@ -100,7 +97,7 @@ function nextQuestion() {
            
     }
 
-
+// Turns off display for some areas, displays the user's score 
 function finalScore() {
     quizContainer.style.display = "none";
     praise.style.display = "none";
@@ -112,6 +109,7 @@ function finalScore() {
 
 }
 
+// Gets values stored in local storage to be used in displaying score, clears out intro text area, creates play again button, adds listener event for the play again button
 function showRecordedScore () {
     var initials = localStorage.getItem("initials");
     var endScore = localStorage.getItem("endScore");
@@ -125,12 +123,11 @@ function showRecordedScore () {
     document.body.appendChild(playAgainButton);
     playAgainButton.addEventListener("click", function(event) {
         event.preventDefault();
-        console.log("reload");
         location.reload();
     })
 }
 
-
+// Sets local storage values for initials and score 
 var initialsInput = document.querySelector("#initials");
 var initialsButton = document.querySelector("#submitInitials");
 
